74662cf985d978f7f023d9218b625804468decbd
PlaidDroid <plaiddroid@gmail.com>
2026-07-11 15:19:39 +0530

    feat: update Vite version, add RSVP page, and configure build options

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path'
 
 export default defineConfig({
+	build: {
+		rolldownOptions: {
+			input: {
+				main: resolve(import.meta.dirname, 'index.html'),
+				rsvp: resolve(import.meta.dirname, 'rsvp/index.html'),
+			},
+		},
+	},
 	plugins: [cloudflare()],
 });
